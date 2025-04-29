54176416496fa9b0b04974b11c25baf31c6dccd8
Travis Souther <souther-trav@hotmail.com>
2025-04-28 20:58:36 -0500

    added likes controller and routing

--- routes/likes.js
+++ routes/likes.js
@@ -1,55 +1,11 @@
-// const express = require("express");
-// const { PrismaClient } = require("../generated/prisma");
-// const prisma = new PrismaClient();
+const express = require("express");
+const router = express.Router();
+const likesController = require("../controllers/likesController")
 
-// const router = express.Router();
+// Like a challenge
+router.post("/:commentId", likesController.addLikeToComment );
 
-// // create a like
-// router.post("/likes", async function (req, res, next) {
-//     try {
-//         const { user_id, comment_id, challenge_id} = req.body;
+//remove like
+router.delete("/:commentId", likesController.removeLike);
 
-//         const newLike = await prisma.likes.create({
-//             data: { 
-//                 user_id,
-//                 comment_id,
-//                 challenge_id,
-//             },
-//             select: {
-//                 id: true,
-//                 user_id: true,
-//                 comment_id: true,
-//                 challenge_id: true,
-//                 created_at: true
-//             }
-//         });
-
-//         res.status(201).json(newLike);
-//     } catch (error) {
-//         next(error);
-//     }
-// });
-// // route to remove a like
-// router.delete("/likes/:id", async function (req, res, next) {
-//     try {
-//         const { id } = req.params;
-
-//         const deletedLike = await prisma.likes.delete({
-//             // primsa expects a number, not a string.
-//             where: {id: parseInt(id)},
-//             select: {
-//                 id: true,
-//                 user_id: true,
-//                 comment_id: true,
-//                 challenge_id: true,
-//                 created_at: true
-//             }
-//         })
-//         res.json({message: "Removed like", deletedLike})
-//     } catch (error) {
-//         console.error(error);
-//         res.status(404).json({error: "No like found, failed to delete"})
-//     }
-// });
-
-// module.exports = router;
+module.exports = router;
